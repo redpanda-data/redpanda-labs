83634bf4e97043f8c238b07f1a6778ed5128e73c
James Kinley <james@redpanda.com>
2023-10-27 10:53:43 +0100

    Switched to confluent-kafka

--- clients/python/producer.py
+++ clients/python/producer.py
@@ -5,19 +5,32 @@ import logging
 import pathlib
 
 from dotenv import load_dotenv
-from kafka import KafkaAdminClient, KafkaProducer
-from kafka.admin import NewTopic
-from kafka.errors import TopicAlreadyExistsError
+from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient, NewTopic, KafkaException
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 redpanda_brokers = os.getenv("REDPANDA_BROKERS")
-kafka_security_protocol = os.getenv("KAFKA_SECURITY_PROTOCOL", "PLAINTEXT")
-kafka_sasl_mechanism = os.getenv("KAFKA_SASL_MECHANISM", None)
-redpanda_username = os.getenv("REDPANDA_USERNAME", None)
-redpanda_password = os.getenv("REDPANDA_PASSWORD", None)
+kafka_security_protocol = os.getenv("KAFKA_SECURITY_PROTOCOL")
+kafka_sasl_mechanism = os.getenv("KAFKA_SASL_MECHANISM")
+redpanda_username = os.getenv("REDPANDA_USERNAME")
+redpanda_password = os.getenv("REDPANDA_PASSWORD")
 logging.info("Connecting to: %s", redpanda_brokers)
+
+conf = {"bootstrap.servers": redpanda_brokers}
+if kafka_security_protocol and "SASL" in kafka_security_protocol:
+    conf.update(
+        {
+            "security.protocol": kafka_security_protocol,
+            "sasl_mechanism": kafka_sasl_mechanism,
+            "sasl_plain_username": redpanda_username,
+            "sasl_plain_password": redpanda_password,
+            # "ssl_cafile": "ca.crt",
+            # "ssl_certfile": "client.crt",
+            # "ssl_keyfile": "client.key"
+        }
+    )
 
 #
 # Create topic
@@ -25,54 +38,47 @@ logging.info("Connecting to: %s", redpanda_brokers)
 topic_name = os.getenv("REDPANDA_TOPIC_NAME", "test-topic")
 logging.info("Creating topic: %s", topic_name)
 
-admin = KafkaAdminClient(
-    bootstrap_servers=redpanda_brokers,
-    security_protocol=kafka_security_protocol,
-    sasl_mechanism=kafka_sasl_mechanism,
-    sasl_plain_username=redpanda_username,
-    sasl_plain_password=redpanda_password,
-    # ssl_cafile="ca.crt",
-    # ssl_certfile="client.crt",
-    # ssl_keyfile="client.key"
-)
+admin = AdminClient(conf)
 try:
-    admin.create_topics(
-        new_topics=[NewTopic(name=topic_name, num_partitions=1, replication_factor=1)]
-    )
-except TopicAlreadyExistsError as e:
+    topic = NewTopic(topic=topic_name, num_partitions=1, replication_factor=1)
+    admin.create_topics([topic])
+except KafkaException as e:
     logging.error(e)
-finally:
-    admin.close()
+
 
 #
 # Write to topic
 #
-logging.info("Writing to topic: %s", topic_name)
-producer = KafkaProducer(
-    bootstrap_servers=redpanda_brokers,
-    security_protocol=kafka_security_protocol,
-    sasl_mechanism=kafka_sasl_mechanism,
-    sasl_plain_username=redpanda_username,
-    sasl_plain_password=redpanda_password,
-    # ssl_cafile="ca.crt",
-    # ssl_certfile="client.crt",
-    # ssl_keyfile="client.key"
-)
+def delivery_report(err, msg):
+    """Logs the success or failure of message delivery."""
+    if err is not None:
+        logging.error("Unable to deliver message: %s", err)
+    else:
+        out = f"topic: {msg.topic()}, "
+        out += f"partition: {msg.partition()}, "
+        out += f"offset: {msg.offset()}, "
+        out += f"value: {msg.value()}"
+        logging.info(out)
 
-try:
-    here = pathlib.Path(__file__).parent.resolve()
-    for file in glob.glob(f"{here}/../data/*.csv"):
-        logging.info("Processing file: %s", file)
-        with open(file, encoding="utf-8") as csvfile:
-            reader = csv.reader(csvfile)
-            next(reader, None)  # skip the header
-            for row in reader:
-                msg = ",".join(row)
-                f = producer.send(topic_name, str.encode(msg))
-                f.get(timeout=100)
-                logging.info("Produced: %s", msg)
-except Exception as e:
-    logging.error(e)
-finally:
-    producer.flush()
-    producer.close()
+
+logging.info("Writing to topic: %s", topic_name)
+p = Producer(conf)
+
+here = pathlib.Path(__file__).parent.resolve()
+for file in glob.glob(f"{here}/../data/*.csv"):
+    logging.info("Processing file: %s", file)
+    with open(file, encoding="utf-8") as f:
+        reader = csv.reader(f)
+        next(reader, None)  # skip the header
+        for row in reader:
+            val = ",".join(row)
+            try:
+                p.produce(
+                    topic=topic_name,
+                    value=val.encode("utf-8"),
+                    on_delivery=delivery_report,
+                )
+            except KafkaException as e:
+                logging.error(e)
+
+p.flush()
