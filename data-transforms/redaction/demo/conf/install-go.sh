411dfe0653567f081f89bd8044a07f1466837183
JakeSCahill <jake@redpanda.com>
2024-03-27 17:10:57 +0000

    Fix install-go.sh

--- data-transforms/redaction/demo/conf/install-go.sh
+++ data-transforms/redaction/demo/conf/install-go.sh
@@ -2,7 +2,7 @@ apt -y update
 apt -y install wget
 
 cd /tmp || exit
-ARCH=$(arch | sed 's/aarch64/arm64/')
+ARCH=$(arch | sed 's/aarch64/arm64/' | sed 's/x86_64/amd64/')
 
 wget https://go.dev/dl/go1.21.5.linux-${ARCH}.tar.gz || exit
 tar xf go1.21.5.linux-${ARCH}.tar.gz
